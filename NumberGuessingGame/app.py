bc9be81095dd7d799d8fe33d93c22d7d407edee2
Krishna Kamal Baishnab <kamalkrishna98@gmail.com>
2024-10-14 17:57:55 +0100

    Number GuessingGame

---
+++ NumberGuessingGame/app.py
@@ -0,0 +1,34 @@
+# import random
+
+# number = random.randint(1,100)
+
+# playerNumber = int(input("Guess The Number between 1 and 100 : "))
+
+# if playerNumber > number:
+#     print("Too High , Guess Again")
+# elif playerNumber < number:
+#     print("Too Low, Guess Again")
+# elif playerNumber == number:
+#     print("Congratulations, You guessed the number")
+    
+
+# print(number)
+
+
+
+import random
+
+number = random.randint(1, 100)
+
+while True:
+    try:
+        playerNumber = int(input("Guess the number between 1 and 100: "))
+        if playerNumber > number:
+            print("Too High, Guess Again")
+        elif playerNumber < number:
+            print("Too Low, Guess Again")
+        else:
+            print("Congratulations, You guessed the number!")
+            break  # Exit the loop when the correct number is guessed
+    except ValueError:
+        print("Please Enter a valid Number!")
